9cdba5418b3c9455f262c709e016f33c0a2a894d
Robert Russo <rrusso44@gmail.com>
2015-11-14 16:15:38 -0500

    Routing!
    We can now route correctly

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 	require "resources/connect.php";
+	require 'vendor/autoload.php';
 
 	try {
 		$conn = new PDO("mysql:host=$server;", $username, $password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	echo "Connected successfully";
 	} catch(PDOException $e) {
     	echo "Connection failed: " . $e->getMessage();
     }
 
-    $init = "CREATE DATABASE IF NOT EXISTS volunteer";
-    $conn->exec($init);
+    $app = new \Slim\Slim();
+    $app->get('/', function() {
+    	echo "You're home!";
+    });
+    $app->get('/hello/:name', function ($name) {
+    	echo "Hello, " . $name;
+	});
+	$app->run();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>RPI Hackathan Volunteer App</title>
-</head>
-<body>
-	<h1>Testing the application. Yes it works!</h1>
-</body>
-</html>
