3cab211d76145f5b34f57092ac8a64e63b2f0dab
Justin Etzine <justetz@gmail.com>
2015-11-15 10:50:15 -0500

    Ng-repeat!

--- public_html/js/controllers/ListingController.js
+++ public_html/js/controllers/ListingController.js
@@ -2,6 +2,7 @@ app.controller('ListingController',  ['$scope', '$http', function($scope, $http)
 	$scope.data = [];
 	var loadData = function() {
 		$http.get('api/listings/').success(function(response) {
+			$scope.data = response;
 			console.log(response);
 			// success callback
 		}).error(function(response) {
